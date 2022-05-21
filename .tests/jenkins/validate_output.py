861e33879ca93dd594064c63cdef42f011d0f196
Patrik Smeds <patrik.smeds@gmail.com>
2022-05-21 22:42:07 +0200

    fix: update subprocess command

--- .tests/jenkins/validate_output.py
+++ .tests/jenkins/validate_output.py
@@ -106,7 +106,8 @@ def validate_result(json_file, directory):
 
 
 def process_gz_file(f_path, directory):
-    command = ["zcat", os.path.join(directory, f_path), "|", "paste - - - -", "|", "sort", "-k1", "-S 5G", "|", "md5sum"]
+    #command = ["zcat", os.path.join(directory, f_path), "|", "paste - - - -", "|", "sort", "-k1", "-S 5G", "|", "md5sum"]
+    command = ["zcat", os.path.join(directory, f_path), "|", "md5sum"]
     return subprocess.run(" ".join(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 
